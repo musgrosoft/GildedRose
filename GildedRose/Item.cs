95c43b5b3cf2ee9b68f1503e1b6f8b553b81a521
Tim Musgrove <tim.musgrove@stepstone.com>
2022-11-10 11:29:03 +0000

    Initial commit

---
+++ GildedRose/Item.cs
@@ -0,0 +1,55 @@
+﻿namespace GildedRoseKata
+{
+    public interface IItem
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public void Update();
+    }
+
+    public  class Item : IItem
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    // public abstract class Item
+    // {
+    //     public string Name { get; set; }
+    //     public int SellIn { get; set; }
+    //     public int Quality { get; set; }
+    //
+    //     public abstract void Update();
+    // }
+
+
+    public class AgedBrie : IItem
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public void Update()
+        {
+            SellIn = SellIn - 1;
+
+            if (Quality < 50)
+            {
+                Quality = Quality + 1;
+            }
+
+            if (SellIn < 0 && Quality < 50)
+            {
+                Quality = Quality + 1;
+            }
+        }
+
+    }
+}
